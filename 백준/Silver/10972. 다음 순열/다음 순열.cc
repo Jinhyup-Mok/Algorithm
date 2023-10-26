91285b83e1254223cbcd363c89d32e5dd9740d46
JINHYUP MOK <mokjh1117@gmail.com>
2023-10-26 17:52:39 +0900

    [Silver III] Title: 다음 순열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/다음 순열.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+    int n;
+    int a[10000];
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+    if (next_permutation(a, a + n)) {
+        for (int i = 0; i < n; ++i) {
+            cout << a[i] << " ";
+        }
+    } else {
+        cout << "-1";
+    }
+    cout << "\n";
+}
